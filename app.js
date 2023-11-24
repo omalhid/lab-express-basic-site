ca9fd5d241dea6357e247bf28b77a017706c1467
Omayma El Hadari <om.elhadari@gmail.com>
2023-11-24 20:45:39 +0100

    In process

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const app = express();
+const port = 4000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/home.html`)
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(`${__dirname}/views/about.html`)
+})
+
+app.get('/works', (req, res) => {
+    res.sendFile(`${__dirname}/views/works.html`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+
