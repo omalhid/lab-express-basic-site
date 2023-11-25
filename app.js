4df92ac8f35ab5e288e1abf60faaead9522761b0
Omayma El Hadari <om.elhadari@gmail.com>
2023-11-25 19:59:09 +0100

    finished home and about page

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require ('express');
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(express.static('public'));
 
+//home page route
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/views/home.html`)
 })
 
+//about page route
 app.get('/about', (req, res) => {
     res.sendFile(`${__dirname}/views/about.html`)
 })
 
+//works page route
 app.get('/works', (req, res) => {
     res.sendFile(`${__dirname}/views/works.html`)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Paramore App listening on port ${port}`)
 })
 
 
